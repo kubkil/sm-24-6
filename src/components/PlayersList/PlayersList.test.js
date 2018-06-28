9a913f4b2c7017dcfb7be866a1731f82038086a2
Jakub Kilar <kuba.kilar@gmail.com>
2018-06-28 21:19:47 +0200

    test rendering correct number of players

--- src/components/PlayersList/PlayersList.test.js
+++ src/components/PlayersList/PlayersList.test.js
@@ -4,4 +4,22 @@ import { shallow } from 'enzyme';
 
 it('renders without crashing', () => {
   shallow(<PlayersList players={[]} />);
+});hallow(<PlayersList players={[]} />);
 });
+
+it('renders correct number of players', () => {
+  const players = [
+    {
+      name: 'Kunegunda',
+      score: 5
+    },
+    {
+      name: 'Antoś',
+      score: 0
+    }
+  ]
+  const playerComponent = shallow(<PlayersList players={players} />);
+  const expectedPlayersNumber = playerComponent.find(Player).length;
+
+  expect(expectedPlayersNumber).toEqual(2);
+})
