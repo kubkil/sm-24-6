e17423315037473fd6066c33b1efc9cce6868781
Jakub Kilar <kuba.kilar@gmail.com>
2018-06-28 21:20:08 +0200

    test calling onScoreUpdate

--- src/components/PlayersList/PlayersList.test.js
+++ src/components/PlayersList/PlayersList.test.js
@@ -1,10 +1,10 @@
 import PlayersList from './PlayersList';
 import React from 'react';
 import { shallow } from 'enzyme';
+import Player from '../Player/Player';
 
 it('renders without crashing', () => {
   shallow(<PlayersList players={[]} />);
-});hallow(<PlayersList players={[]} />);
 });
 
 it('renders correct number of players', () => {
@@ -23,3 +23,24 @@ it('renders correct number of players', () => {
 
   expect(expectedPlayersNumber).toEqual(2);
 })
+
+it('calls onScoreUpdate', () => {
+  const players = [
+    {
+      name: 'Kunegunda',
+      score: 5
+    },
+    {
+      name: 'Antoś',
+      score: 0
+    }
+  ]
+
+  const mockedOnScoreUpdate = jest.fn();
+  const playerComponent = shallow(<PlayersList players={players} onScoreUpdate={mockedOnScoreUpdate} />);
+  const firstPlayer = playerComponent.find(Player).first();
+  const onPlayerScoreChange = firstPlayer.prop('onPlayerScoreChange');
+  onPlayerScoreChange(10);
+
+  expect(mockedOnScoreUpdate).toBeCalledWith(0, 10);
+});
