02eb94ef3e646bdd062da14548b405e68ee00daa
Jakub Kilar <kuba.kilar@gmail.com>
2018-06-29 21:16:04 +0200

    add test for player score

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,26 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
+import PlayersList from './components/PlayersList/PlayersList';
 
 it('renders without crashing', () => {
   shallow(<App />);
+});
+
+it('should update player score', () => {
+  const players = [
+    {
+      name: 'Kunegunda',
+      score: 5
+    }
+  ];
+  const appComponent = shallow(<App />);
+  appComponent.setState({ players });
+
+  const onScoreUpdate = appComponent.find(PlayersList).prop('onScoreUpdate');
+  onScoreUpdate(0, 5);
+
+  const playersAfterUpdate = appComponent.state('players');
+
+  expect(playersAfterUpdate[0].score).toEqual(10);
 });
