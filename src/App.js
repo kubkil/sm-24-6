0ec264e1d91fba9b81d7355fd18e72c80ac6479d
Jakub Kilar <kuba.kilar@gmail.com>
2018-06-30 13:20:16 +0200

    add AddPlayer component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import PlayersList from './components/PlayersList/PlayersList';
+import AddPlayer from './components/AddPlayer/AddPlayer';
 
 class App extends Component {
   constructor() {
@@ -25,9 +26,20 @@ class App extends Component {
     })
   }
 
+  onPlayerAdd = (playerName) => {
+    const newPlayer = {
+      name: playerName,
+      score: 0,
+    }
+    this.setState({
+      players: [...this.state.players, newPlayer]
+    })
+  }
+
   render() {
     return (
       <div className="App">
+        <AddPlayer onPlayerAdd={this.onPlayerAdd} />
         <PlayersList players={this.state.players} onScoreUpdate={this.onScoreUpdate} />
       </div>
     );
