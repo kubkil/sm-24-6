cd1258aec2f5a16cdf7c1161b11d38aa9eec780b
Jakub Kilar <kuba.kilar@gmail.com>
2018-06-30 16:40:42 +0200

    test deleting a player

--- src/App.test.js
+++ src/App.test.js
@@ -2,19 +2,20 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
 import PlayersList from './components/PlayersList/PlayersList';
+import AddPlayer from './components/AddPlayer/AddPlayer';
 
-it('renders without crashing', () => {
+it('should render without crashing', () => {
   shallow(<App />);
 });
 
 it('should update player score', () => {
+  const appComponent = shallow(<App />);
   const players = [
     {
       name: 'Kunegunda',
       score: 5
     }
-  ];
-  const appComponent = shallow(<App />);
+  ]
   appComponent.setState({ players });
 
   const onScoreUpdate = appComponent.find(PlayersList).prop('onScoreUpdate');
@@ -23,7 +24,6 @@ it('should update player score', () => {
   const playersAfterUpdate = appComponent.state('players');
 
   expect(playersAfterUpdate[0].score).toEqual(10);
-});
 });
 
 it('should add new player and update player state', () => {
@@ -35,4 +35,27 @@ it('should add new player and update player state', () => {
   expect(players.length).toEqual(1);
   expect(players[0].name).toEqual('Ania');
   expect(players[0].score).toEqual(0);
+});
+
+it('should delete a player and update player state', () => {
+  const appComponent = shallow(<App />);
+  const players = [
+    {
+      name: 'Kunegunda',
+      score: 5
+    },
+    {
+      name: 'abc',
+      score: 5
+    },
+  ]
+  appComponent.setState({ players });
+  const onPlayerRemove = appComponent.find(PlayersList).prop('onPlayerRemove');
+  console.log(onPlayerRemove);
+  onPlayerRemove(1);
+  console.log(onPlayerRemove());
+  const playersAfterUpdate = appComponent.state('players');
+  console.log(playersAfterUpdate);
+  console.log(playersAfterUpdate.length);
+  expect(playersAfterUpdate.length).toEqual(1);
 });
