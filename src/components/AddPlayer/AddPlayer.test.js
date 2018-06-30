6a90d536594b8174eaffbf67222edab351594b93
Jakub Kilar <kuba.kilar@gmail.com>
2018-06-30 13:09:26 +0200

    create tests for AddPlayer - basic render and adding name

---
+++ src/components/AddPlayer/AddPlayer.test.js
@@ -0,0 +1,19 @@
+import AddPlayer from './AddPlayer';
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+
+it('renders without crashing', () => {
+  shallow(<AddPlayer />);
+});
+
+it('calls onPlayerAdd with correct name', () => {
+  const mockedOnPlayerAdd = jest.fn();
+  // onPlayerAdd? dlaczego nie onSubmit? AddPlayer linia 10
+  const addPlayerComponent = mount(<AddPlayer onPlayerAdd={mockedOnPlayerAdd} />);
+  // getDOMNode() działa tylko w trybie mount
+  const nameInput = addPlayerComponent.find('input').first().getDOMNode();
+  nameInput.value = 'Ania';
+  const form = addPlayerComponent.find('form');
+  form.simulate('submit');
+  expect(mockedOnPlayerAdd).toBeCalledWith('Ania');
+});
