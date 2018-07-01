4c8d289c17219ca23cc4bd72ecf4809e79321c86
Jakub Kilar <kuba.kilar@gmail.com>
2018-07-01 13:54:03 +0200

    add fontAwesome icons

--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './Player.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const Player = (props) => (
   <li className="Player">
     <span className="Player__name">{props.name}</span>
     <span className="Player__score">{props.score}</span>
-    <span className="Player__button" onClick={() => props.onPlayerScoreChange(1)} >+</span>
-    <span className="Player__button" onClick={() => props.onPlayerScoreChange(-1)} >-</span>
-    <span className="Player__remove" onClick={() => props.onPlayerRemove()} >REMOVE</span>
+    <span className="Player__button" onClick={() => props.onPlayerScoreChange(1)} ><FontAwesomeIcon icon="plus" className="Icon" /></span>
+    <span className="Player__button" onClick={() => props.onPlayerScoreChange(-1)} ><FontAwesomeIcon icon="minus" className="Icon" /></span>
+    <span className="Player__remove" onClick={() => props.onPlayerRemove()} ><FontAwesomeIcon icon="trash-alt" className="Icon" /></span>
   </li>
 );
 
