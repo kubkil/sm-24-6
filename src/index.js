bbf32bb05d23d3a62020ec305f866584c252219f
Jakub Kilar <kuba.kilar@gmail.com>
2018-07-01 13:53:28 +0200

    import WebFont and load google font

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import WebFont from 'webfontloader';
+
+WebFont.load({
+  google: {
+    families: ['Libre Baskerville:400,700', 'serif']
+  }
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
