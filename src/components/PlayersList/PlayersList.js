d30ebb1a81cd3d4c9e8e2fd1fa2ceba238824553
Jakub Kilar <kuba.kilar@gmail.com>
2018-07-01 13:55:07 +0200

    add heading list item

--- src/components/PlayersList/PlayersList.js
+++ src/components/PlayersList/PlayersList.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import Player from '../Player/Player';
 import './PlayersList.css';
+import '../Player/Player.css';
 
 const PlayersList = (props) => (
   <ul className="PlayersList">
     {/* players? app.js players? idzie z App w dół*/}
+    <li className="Player">
+      <span className="Heading">Name</span>
+      <span className="Heading">Points</span>
+      <span className="Heading">Add points</span>
+      <span className="Heading">Remove points</span>
+      <span className="Heading">Remove player</span>
+    </li>
     {props.players.map((player, i) => (
       <Player
         key={i}
